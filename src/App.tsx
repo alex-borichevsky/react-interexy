429e103dd7a2b17721a08cea40fe1dca59ddce93
Алексей <borichevskiy03@mail.ru>
2023-02-20 21:00:40 +0300

    bugs

--- src/App.tsx
+++ src/App.tsx
@@ -5,16 +5,12 @@ import { Route, Routes } from 'react-router-dom';
 import ButtonComponent from "./components/Button";
 import Main from "./components/Main";
 import {ProductPage} from "./pages/ProductPage";
-import {ReduxPage} from "./pages/ReduxPage";
-import Form from "./components/form/Form";
 
 function App() {
   return (
       <Routes>
           <Route path="/main" element={<Main/>}/>
           <Route path='/products' element={<ProductPage/>}></Route>
-          <Route path='/redux' element={<ReduxPage/>}></Route>
-          <Route path='/form' element={<Form/>}></Route>
       </Routes>
   );
 }
